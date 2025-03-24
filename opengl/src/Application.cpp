537311b32ed75773c542c0e79fed49073a1155f8
honzajanecek <honzajanecek@centrum.cz>
2025-03-24 19:11:05 +0100

    uu

--- opengl/src/Application.cpp
+++ opengl/src/Application.cpp
@@ -18,41 +18,49 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
-float NDC(const unsigned int& windowSize, unsigned int& pixelPosition) {
+/*float NDC(int& windowSize, unsigned int& pixelPosition) {
 
     return ((pixelPosition * 1.0f / windowSize) * 2.0f - 1.0f);
-}
+}*/
 
-const unsigned int xWindowSize = 640; //has to be multiple of 10
-const unsigned int yWindowSize = 480;
+int xWindowSize = 960;
+int yWindowSize = 540;
 
-const unsigned int gridSpacing = 20; //has to divide windowsize without remainder
+//const unsigned int gridSpacing = 20;
 
-std::vector<float> GenerateGridVertices(const unsigned int& xWindowSize, const unsigned int& yWindowSize, const unsigned int& gridSpacing) {
+/*std::vector<float> GenerateGridVertices(int& xWindowSize, int& yWindowSize, const unsigned int& gridSpacing) {
 
     std::vector<float> vertices;
 
     // x axis
     for (unsigned int i = gridSpacing; i < xWindowSize; i += gridSpacing) {
 
-        vertices.push_back(NDC(xWindowSize, i));
-        vertices.push_back(-1.0f);
+        vertices.push_back(i * 1.0f);
+        vertices.push_back(0.0f);
+        vertices.push_back(0.0f);
+        vertices.push_back(0.0f);
 
-        vertices.push_back(NDC(xWindowSize,i));
-        vertices.push_back(1.0f);
+        vertices.push_back(i * 1.0f);
+        vertices.push_back(yWindowSize * 1.0f);
+        vertices.push_back(0.0f);
+        vertices.push_back(0.0f);
     }
 
     //y axis
     for (unsigned int i = gridSpacing; i < yWindowSize; i += gridSpacing) {
 
-        vertices.push_back(-1.0f);
-        vertices.push_back(NDC(yWindowSize, i));
+        vertices.push_back(0.0f);
+        vertices.push_back(i * 1.0f);
+        vertices.push_back(0.0f);
+        vertices.push_back(0.0f);
 
-        vertices.push_back(1.0f);
-        vertices.push_back(NDC(yWindowSize, i));
+        vertices.push_back(xWindowSize * 1.0f);
+        vertices.push_back(i * 1.0f);
+        vertices.push_back(0.0f);
+        vertices.push_back(0.0f);
     }
     return vertices;
-}
+}*/
 
 int main(void)
 {
@@ -85,25 +93,36 @@ int main(void)
 
     /*scope*/
     {
+        
+        //std::vector<float> positions = GenerateGridVertices(xWindowSize, yWindowSize, gridSpacing);
 
-        std::vector<float> positions = GenerateGridVertices(xWindowSize, yWindowSize, gridSpacing);
+        float positions[] = {
+            100.0f,100.0f,0.0f,0.0f,
+            200.0f,100.0f,0.0f,0.0f,
+            200.0f,200.0f,0.0f,0.0f
+        };
+
+        unsigned int indicies[] = {
+            0,1,2
+        };
 
         /*vertex array*/
         VertexArray va;
 
         /*vertex buffer*/
-        VertexBuffer vb(positions.data(), positions.size() * sizeof(float));
+        VertexBuffer vb(positions, 12 * sizeof(float));
 
         /*vertex buffer layout*/
         VertexBufferLayout layout;
         layout.Push<float>(2);
+        layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
         /*index buffer*/
-        //IndexBuffer ib(indicies, 6);
+        IndexBuffer ib(indicies, 3);
 
         /*projection matrix*/
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, xWindowSize * 1.0f, 0.0f, yWindowSize * 1.0f, -1.0f, 1.0f);
 
         /*shader*/
         Shader shader("res/shaders/Basic.shader");
@@ -114,7 +133,7 @@ int main(void)
         va.Unbind();
         shader.Unbind();
         vb.Unbind();
-        //ib.Unbind();
+        ib.Unbind();
 
         /*renderer*/
         Renderer renderer;
@@ -132,10 +151,10 @@ int main(void)
             shader.SetUniform4f("u_Color", red, 0.3f, 0.8f, 1.0f);
             shader.SetUniformMat4f("u_MVP", proj);
 
-            //renderer.DrawTriangles(va, ib, shader);
+            renderer.DrawTriangles(va, ib, shader);
 
-            unsigned int count = ((xWindowSize + yWindowSize) / gridSpacing * 2) - 2 * 2;
-            renderer.DrawLines(va, shader, count);
+            //unsigned int count = (positions.size()/4);
+            //renderer.DrawLines(va, shader, count);
 
             /*change red channel*/
             if (red > 1.0f) {
