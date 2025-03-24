6b26c7dff4576cc78f7321c64818109876d7ea8a
honzajanecek <honzajanecek@centrum.cz>
2025-03-24 16:49:39 +0100

    hjalp

--- opengl/src/Application.cpp
+++ opengl/src/Application.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 #include "Renderer.h"
 #include "VertexBuffer.h"
@@ -14,6 +15,44 @@
 #include "VertexBufferLayout.h"
 #include "Shader.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+float NDC(const unsigned int& windowSize, unsigned int& pixelPosition) {
+
+    return ((pixelPosition * 1.0f / windowSize) * 2.0f - 1.0f);
+}
+
+const unsigned int xWindowSize = 640; //has to be multiple of 10
+const unsigned int yWindowSize = 480;
+
+const unsigned int gridSpacing = 20; //has to divide windowsize without remainder
+
+std::vector<float> GenerateGridVertices(const unsigned int& xWindowSize, const unsigned int& yWindowSize, const unsigned int& gridSpacing) {
+
+    std::vector<float> vertices;
+
+    // x axis
+    for (unsigned int i = gridSpacing; i < xWindowSize; i += gridSpacing) {
+
+        vertices.push_back(NDC(xWindowSize, i));
+        vertices.push_back(-1.0f);
+
+        vertices.push_back(NDC(xWindowSize,i));
+        vertices.push_back(1.0f);
+    }
+
+    //y axis
+    for (unsigned int i = gridSpacing; i < yWindowSize; i += gridSpacing) {
+
+        vertices.push_back(-1.0f);
+        vertices.push_back(NDC(yWindowSize, i));
+
+        vertices.push_back(1.0f);
+        vertices.push_back(NDC(yWindowSize, i));
+    }
+    return vertices;
+}
 
 int main(void)
 {
@@ -24,7 +63,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(xWindowSize, yWindowSize, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -43,24 +82,17 @@ int main(void)
     /*check if GLEW is init*/
     if (glewInit() != GLEW_OK)
         std::cout << "err" << std::endl;
-    {
-        float positions[] = {
-            -0.5f, -0.5f,   //0
-             0.5f, -0.5f,   //1
-             0.5f,  0.5f,   //2
-            -0.5f,  0.5f    //3
-        };
 
-        unsigned int indicies[] = {
-            0,1,2,
-            2,3,0
-        };
+    /*scope*/
+    {
+
+        std::vector<float> positions = GenerateGridVertices(xWindowSize, yWindowSize, gridSpacing);
 
         /*vertex array*/
         VertexArray va;
 
         /*vertex buffer*/
-        VertexBuffer vb(positions, 2 * 4 * sizeof(float));
+        VertexBuffer vb(positions.data(), positions.size() * sizeof(float));
 
         /*vertex buffer layout*/
         VertexBufferLayout layout;
@@ -68,7 +100,10 @@ int main(void)
         va.AddBuffer(vb, layout);
 
         /*index buffer*/
-        IndexBuffer ib(indicies, 6);
+        //IndexBuffer ib(indicies, 6);
+
+        /*projection matrix*/
+        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
 
         /*shader*/
         Shader shader("res/shaders/Basic.shader");
@@ -79,7 +114,7 @@ int main(void)
         va.Unbind();
         shader.Unbind();
         vb.Unbind();
-        ib.Unbind();
+        //ib.Unbind();
 
         /*renderer*/
         Renderer renderer;
@@ -95,8 +130,12 @@ int main(void)
 
             shader.Bind();
             shader.SetUniform4f("u_Color", red, 0.3f, 0.8f, 1.0f);
+            shader.SetUniformMat4f("u_MVP", proj);
 
-            renderer.Draw(va, ib, shader);
+            //renderer.DrawTriangles(va, ib, shader);
+
+            unsigned int count = ((xWindowSize + yWindowSize) / gridSpacing * 2) - 2 * 2;
+            renderer.DrawLines(va, shader, count);
 
             /*change red channel*/
             if (red > 1.0f) {
--- opengl/src/Renderer.cpp
+++ opengl/src/Renderer.cpp
@@ -21,7 +21,7 @@ void Renderer::Clear() const
     GLCall(glClear(GL_COLOR_BUFFER_BIT));
 }
 
-void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const
+void Renderer::DrawTriangles(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const
 {
     shader.Bind();
     va.Bind();
@@ -29,3 +29,11 @@ void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& 
 
     GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
 }
+
+void Renderer::DrawLines(const VertexArray& va, const Shader& shader, const unsigned int& count) const
+{
+    shader.Bind();
+    va.Bind();
+    GLCall(glDrawArrays(GL_LINES, 0, count));
+}
+
