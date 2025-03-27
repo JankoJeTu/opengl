a0b3057497c394dc86cc877c8dfade894061329b
Jan Janeček <XFJanec@gysms.cz>
2025-03-27 12:41:08 +0100

    peepoo

--- opengl/src/Application.cpp
+++ opengl/src/Application.cpp
@@ -22,17 +22,41 @@
 int xWindowSize = 800;
 int yWindowSize = 800;
 
-struct Circle {
+const float gravity = -0.5f; // g acceleration
+const float dt = 0.016f; //frame
+float bounce = 1.005f; //energy loss
 
-    float x, y;        // Position
-    float vx, vy;      // Velocity
-    float radius;      // Radius
+
+struct RigidBody {
+
+    glm::vec2 position;
+    glm::vec2 velocity;
+    glm::vec2 acceleration;
+    float radius;
+    float mass;
+
+    RigidBody(glm::vec2 pos, glm::vec2 v, glm::vec2 a, float r, float m)
+        : position(pos), velocity(v), acceleration(a), radius(r), mass(m) {}
+
 };
 
-std::vector<Circle> circles = {
+void updatePhysics(RigidBody& rigidBody, const float& dt, float& bounce) {
 
-    { 0.0f, 0.0f, 0.0f, 0.0f, 0.4f }   // Circle 3
-};
+    rigidBody.velocity.y += gravity * dt; //gravity
+    rigidBody.position.y += rigidBody.velocity.y * dt; //update position
+
+   /*assuming screen height - 1 to 1*/
+    if (rigidBody.position.y - rigidBody.radius < -1.0f) {
+        rigidBody.position.y = -1.0f + rigidBody.radius; //stop at ground
+        rigidBody.velocity.y *= -bounce; //energy loss
+    }
+
+    else if (rigidBody.position.y + rigidBody.radius > 1.0f) {
+        rigidBody.position.y = 1.0f - rigidBody.radius; //stop at ceiling
+        rigidBody.velocity.y *= -bounce; //energy loss
+    }
+}
+
 
 int main(void)
 {
@@ -50,6 +74,7 @@ int main(void)
         return -1;
     }
 
+    /*opengl version*/
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -65,6 +90,7 @@ int main(void)
 
     /*scope*/
     {
+        /*canvas*/
         float quadVertices[] = {
 
             -1.0f, -1.0f,
@@ -74,6 +100,9 @@ int main(void)
         };
 
         unsigned int indices[] = { 0, 1, 2, 1, 2, 3 };
+
+        /*rb circle*/
+        RigidBody circle(glm::vec2(0.0f, 0.0f), glm::vec2(0.0f, 0.8f), glm::vec2(0.0f, 0.0f), 0.5f, 1.0f);
 
         /*blending*/
         glEnable(GL_BLEND);
@@ -100,7 +129,7 @@ int main(void)
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform3f("u_Color", 0.2f, 0.3f, 0.8f);
-        shader.SetUniform1f("u_Radius", 0.2f);
+        shader.SetUniform1f("u_Radius", 0.5f);
         shader.SetUniform2f("u_Center", 0.0f, 0.0f);
         shader.SetUniform1f("u_EdgeW", 0.005f);
 
@@ -113,30 +142,24 @@ int main(void)
         /*renderer*/
         Renderer renderer;
 
-        float red = 0.0f;
-        float increment = 0.05f;
-
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
+
+            /*update physics*/
+            updatePhysics(circle, dt, bounce);
+
             /* Render here */
             renderer.Clear();
 
             shader.Bind();
-            shader.SetUniform3f("u_Color", 0.3f, 0.3f, 0.8f);
+            shader.SetUniform2f("u_Center", circle.position.x, circle.position.y);  //update pos
+
+            //shader.SetUniform3f("u_Color", 0.3f, 0.3f, 0.8f);
             //shader.SetUniformMat4f("u_MVP", proj);
 
+            /*canvas*/
             renderer.DrawTriangles(va, ib, shader);
-
-            /*change red channel*/
-            if (red > 1.0f) {
-                increment = -0.05f;
-            }
-            else if (red < 0.0f) {
-                increment = 0.05f;
-            }
-
-            red += increment;
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
